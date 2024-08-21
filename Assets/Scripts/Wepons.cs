a2613bce58be1d6c3afb946ab84b5d26c4dbe8af
Arthur020104 <arthur020104@gmail.com>
2024-08-21 15:09:14 -0300

    BUG gun is rotating around the enemy not the player

--- Assets/Scripts/Wepons.cs
+++ Assets/Scripts/Wepons.cs
@@ -7,12 +7,15 @@ public class Wepons : MonoBehaviour
     [Header("Attached to who?")]
     [SerializeField] private GameObject _pivot;
     private DetectLayer _playerDetectWhereToPoint;
+    [Header("Radius to detect?")]
     [SerializeField] private float _radius;
     private Transform _closestEnemy;
     private int frameUpdateCounter = 0, framesNumber = 0;
+    //[Header("Gun things?")]
+   // [SerializeField]private GameObject _gunPrefab;
     void Start()
     {
-        if(!TryGetComponent<DetectLayer>(out _playerDetectWhereToPoint))
+        if(!_pivot.TryGetComponent<DetectLayer>(out _playerDetectWhereToPoint))
         {
             Debug.LogError("Could not get the detectlayer component");
         }
@@ -26,7 +29,16 @@ public class Wepons : MonoBehaviour
     {
         if(frameUpdateCounter >= framesNumber)
         {
-            _closestEnemy = _playerDetectWhereToPoint.closest;
+            if(_playerDetectWhereToPoint.closest !=null )//&&_closestEnemy !=  _playerDetectWhereToPoint.closest)
+            {
+                _closestEnemy = _playerDetectWhereToPoint.closest;
+                Vector3 diff = _pivot.transform.position - _closestEnemy.position;
+                diff /= Mathf.Sqrt(Mathf.Pow(diff.x,2)+Mathf.Pow(diff.y,2));
+                gameObject.transform.position = diff * _radius;
+            }
+            
+                
+            
             frameUpdateCounter = 0;
         }
         frameUpdateCounter++;
